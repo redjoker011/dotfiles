405a4ed70785e0c2751d4d1bda79797d7ffa4a4f
Peter John Alvarado <redjoker011@gmail.com>
2026-06-25 13:58:59 +0800

    feat: add git stash commands

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -25,9 +25,6 @@ starship init fish | source
 # Theme Configuration
 set -g theme_nerd_fonts yes
 
-# Experimental Git Status Preview
-bind \cx _fzf_search_git_status_with_preview
-
 # Activate mise (version manager) if present
 if type -q mise
     mise activate fish | source
@@ -105,6 +102,9 @@ abbr -a grb 'git rebase'
 abbr -a grba 'git rebase --abort'
 abbr -a grbc 'git rebase --continue'
 abbr -a grbi 'git rebase -i'
+abbr -a gstdu 'git stash -k'
+abbr -a gsta 'git stash push'
+abbr -a gstaa 'git stash apply'
 
 # Git Push/Pull (Origin)
 abbr -a ggpur  'git pull --rebase origin (git branch --show-current)'
